980f21f55fceaddcf30dcdae70af7208bb6b4870
AsturianDreams <saulappmultiplataforma@gmail.com>
2016-06-14 14:26:43 +0200

    .

--- src/Servidor.java
+++ src/Servidor.java
@@ -8,8 +8,6 @@ import javax.swing.JOptionPane;
 
 public class  Servidor {
 	static Pantalla p1; //pantalla Swing
-	static boolean ocupado;   // solo puede haber 1 conexion al mismo tiempo aqui controlamos que no pueda haber 2
-	
 	public static int PUERTO = 9097;
 	public static final String APLICACION="RemotEasy";
 	public static boolean enviandoVideo;
@@ -17,7 +15,6 @@ public class  Servidor {
 	static HiloServidor Servicio;
 	
 	public Servidor(){
-		ocupado=false;
 		 p1=new Pantalla();
 		 enviandoVideo=false;
 	}
@@ -25,15 +22,10 @@ public class  Servidor {
 
 
 	public static void IniciarServidor() {
-		
-		if(ocupado==false){ // si no hjay otra conexion entonces nos ponemos a escuchar
+	
 			Servicio = new HiloServidor(p1);
 				Servicio.start();
 				
-			}
-		else{
-			p1.setText("Se intento hacer una conexion sin exito porque ya hay una conexion abierta actualmente");
-		}
 	
 	}//fin iniciar servidor
 
